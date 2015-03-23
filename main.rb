90b8a539eda2a3cf914cbd01f3555c2b9f8a650c
adriangaro <garovat.adrian@gmail.com>
2015-03-23 18:16:29 +0200

    New branch for chipmunk, added colliding and shapes.

--- main.rb
+++ main.rb
@@ -1,21 +1,72 @@
 require 'gosu'
+require 'chipmunk'
+require_relative 'chip-gosu-functions'
 require_relative 'player'
+
+
 
 class GameWindow < Gosu::Window
   def initialize
     super 640, 480, false
     self.caption = "Game Dev"
 
+    @dt = 1.0/60.0
+
+    @space = CP::Space.new
+    @space.damping = 0.8
+
     @ball = Player.new self
+    @ball2 = Player.new self
+
+    @space.add_body @ball.body
+    @space.add_shape @ball.shape
+    @space.add_body @ball2.body
+    @space.add_shape @ball2.shape
+
+    @ball.warp CP::Vec2.new(320, 240)
+    @ball2.warp CP::Vec2.new(520, 190)
   end
 
   def update
-    @ball.warp 150, 50
+    SUBSTEPS.times do
+      @ball.shape.body.reset_forces
+      @ball2.shape.body.reset_forces
+
+      @ball.validate_position
+      @ball2.validate_position
+
+      if button_down? Gosu::KbLeft
+        @ball.turn_left
+        @ball.accelerate_left
+      end
+
+      if button_down? Gosu::KbRight
+        @ball.turn_right
+        @ball.accelerate_right
+      end
+
+      #if button_down? Gosu::KbUp
+      #  if ( (button_down? Gosu::KbRightShift) || (button_down? Gosu::KbLeftShift) )
+      #    @ball.boost
+      #  else
+      #    @ball.accelerate
+      #  end
+      #end
+      @space.step(@dt)
+    end
   end
 
   def draw
     @ball.draw
+    @ball2.draw
   end
+
+  def button_down(id)
+    if id == Gosu::KbEscape
+      close
+    end
+  end
+
 end
 
 window = GameWindow.new
--- player.rb
+++ player.rb
@@ -1,16 +1,67 @@
 require 'gosu'
+require 'chipmunk'
+require_relative 'chip-gosu-functions'
+
+SUBSTEPS = 6
 
 class Player
+  attr_reader :shape, :body
+
   def initialize(window)
     @image = Gosu::Image.new window, "resources/images/ball.png"
-    @x, @y = 100, 100
+
+    @body = CP::Body.new(20.0, 150.0)
+
+    @shape = CP::Shape::Circle.new(body, 64, CP::Vec2.new(0,0))
+    @shape.collision_type = :ball
+
+    @shape.body.p = CP::Vec2.new(0.0, 0.0)
+    @shape.body.v = CP::Vec2.new(0.0, 0.0)
+    @shape.body.a = (3*Math::PI/2.0)
+    @shape.e = 2
   end
 
-  def warp(x, y)
-    @x, @y = x, y
+  def warp(vect)
+    @shape.body.p = vect
+  end
+
+  def turn_left
+    @shape.body.t -= 600.0/SUBSTEPS
+  end
+
+  def turn_right
+    @shape.body.t += 600.0/SUBSTEPS
+  end
+
+  def accelerate
+      @shape.body.apply_force((@shape.body.a.radians_to_vec2 * (3000.0/SUBSTEPS)), CP::Vec2.new(0.0, 0.0))
+  end
+
+  def boost_left
+    @shape.body.apply_force((CP::Vec2.new(-1.0,0.0) * (3000.0)), CP::Vec2.new(0.0, 0.0))
+  end
+
+  def accelerate_left
+    @shape.body.apply_force((CP::Vec2.new(-1.0,0.0) * (3000.0/SUBSTEPS)), CP::Vec2.new(0.0, 0.0))
+  end
+
+  def boost_right
+    @shape.body.apply_force((CP::Vec2.new(1.0,0.0) * (3000.0)), CP::Vec2.new(0.0, 0.0))
+  end
+
+  def accelerate_right
+    @shape.body.apply_force((CP::Vec2.new(1.0,0.0) * (3000.0/SUBSTEPS)), CP::Vec2.new(0.0, 0.0))
+  end
+
+  def boost
+    @shape.body.apply_force((@shape.body.a.radians_to_vec2 * (3000.0)), CP::Vec2.new(0.0, 0.0))
+  end
+  def validate_position
+    l_position = CP::Vec2.new(@shape.body.p.x % 640, @shape.body.p.y % 480)
+    @shape.body.p = l_position
   end
 
   def draw
-    @image.draw @x, @y, 0
+    @image.draw_rot(@shape.body.p.x, @shape.body.p.y, 0, @shape.body.a.radians_to_gosu)
   end
 end
